56c00005359abc353d4674b4227d527d85a3299e
Mouni0825 <mounigandharapu@gmail.com>
2023-11-15 15:04:06 +0530

    service details

---
+++ service details/Services.js
@@ -0,0 +1,13 @@
+// Services.js
+import React from 'react';
+
+const Services = () => {
+  return (
+    <div>
+      <h1>Services</h1>
+      {/* Add content for the services page */}
+    </div>
+  );
+};
+
+export default Services;
